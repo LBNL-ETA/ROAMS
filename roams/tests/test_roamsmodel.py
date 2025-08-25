2e6e3f3120001353d54d2381ab3c8d081da8675d
Evan Neill <eneill@lbl.gov>
2025-08-25 09:14:05 -0700

    update tests, only test outputs as far as saving config

--- roams/tests/test_roamsmodel.py
+++ roams/tests/test_roamsmodel.py
@@ -4,6 +4,7 @@ import logging
 
 from unittest import TestCase
 
+import yaml
 import numpy as np
 
 from roams.conf import RESULT_DIR
@@ -19,6 +20,7 @@ from roams.tests.test_ghgiinput import STATE_GHGI_FILENAME, STATE_PROD_FILENAME,
 def _half_pod(windnorm_em: np.ndarray) -> np.ndarray:
     """
     A function just for these test. Return P = .5 for nonzero emissions.
+    Have to return 1 for everything else.
     """    
     pod = np.ones(windnorm_em.shape)
     pod[windnorm_em>0] = .5
@@ -34,8 +36,8 @@ TEST_CONFIG = {
     "source_file" : SOURCE_FILE,
     "source_id_name" : "source_id",
     "asset_col" : "asset_type",
-    "prod_asset_type" : ("prod",),
-    "midstream_asset_type" : ("midstream",),
+    "prod_asset_type" : ["prod"],
+    "midstream_asset_type" : ["midstream"],
     "coverage_count" : "coverage_count",
     "aerial_em_col" : None,
     "aerial_em_unit" : None,
@@ -58,13 +60,13 @@ TEST_CONFIG = {
     "ghgi_ch4emissions_ngprod_uncertainty_file" : NATNL_NGPROD_UNCERT_GHGI_FILENAME,
     "ghgi_ch4emissions_petprod_file" : NATNL_PETPROD_GHGI_FILENAME,
     "ghgi_ch4emissions_unit" : "kt/yr",
-    "year" : 1,
+    "year" : 1900,
     "state" : "State1",
     "frac_aerial_midstream_emissions": 0.25,
     "gas_composition" : {"C1":.8,"C2":.1,"C3":.1},
     "stratify_sim_sample" : False,
     "n_mc_samples" : 100,
-    "prod_transition_point" : None,
+    "prod_transition_point" : 10,
     "partial_detection_correction" : True,
     "simulate_error" : False,
     "PoD_fn" : "bin",
@@ -72,19 +74,20 @@ TEST_CONFIG = {
     "midstream_transition_point" : 1000,
     "foldername" : "_roamsmodeltest",
     "save_mean_dist" : True,
-    "loglevel" : logging.INFO,
+    "loglevel" : logging.WARNING,
 }
 
 log = logging.getLogger("roams.tests.test_roamsmodel.ROAMSModelTests")
 
-# An error message for probabalistic tests
-_probabalistic_errormsg = (
-    "This probabilistic test observed an event with a small chance "
-    "of occurring under test expectations. If you run the test again "
-    "and it fails, you likely have a problem."
-)
-
 class ROAMSModelTests(TestCase):
+    """
+    A class to test the internal behavior of ROAMSModel, specifically the 
+    generation and treatment of methane emissions size distributions.
+
+    This handles a lot of the behavior, but does exclude the testing of 
+    the sub-MDL midstream estimate, whose computation is tested in 
+    `test_ghgiinput.py`.
+    """
 
     @classmethod
     def setUpClass(cls):
@@ -98,6 +101,9 @@ class ROAMSModelTests(TestCase):
     def tearDownClass(cls):
         # At the end of tests:
         if os.path.exists(cls.model.outfolder):
+            files = os.listdir(cls.model.outfolder)
+            for file in files:
+                os.remove(os.path.join(cls.model.outfolder,file))
             os.rmdir(cls.model.outfolder)
 
     def setUp(self):
@@ -113,9 +119,9 @@ class ROAMSModelTests(TestCase):
         """
         self.assertEqual(self.model.outfolder,os.path.join(RESULT_DIR,"_roamsmodeltest"))
         self.assertEqual(self.model.save_mean_dist,True)
-        self.assertEqual(self.model.loglevel,logging.INFO)
+        self.assertEqual(self.model.loglevel,logging.WARNING)
         self.assertEqual(self.model.log.name,"roams.model.ROAMSModel")
-        self.assertEqual(self.model.log.level,logging.INFO)
+        self.assertEqual(self.model.log.level,logging.WARNING)
         self.assertEqual(self.model.table_outputs,dict())
         self.assertEqual(self.model.well_visit_count,1_000_000_000)
         self.assertEqual(self.model.wells_per_site,3.14159)
@@ -133,42 +139,23 @@ class ROAMSModelTests(TestCase):
         # The simulated sample is not stratified, so the emissions 
         # values are drawn directly from [1,2,3,4,5] (emissions values 
         # in the table)
+        np.random.seed(1)
         sim_sample = self.model.make_simulated_sample()
         
         # Assert the shape is [Num wells to simulate] x [N MC samples]
         self.assertEqual(sim_sample.shape,(1000,100))
 
-        # Assert each underlying value ends up in each sample
-        # And sampled approximately 20% of the time
-        # (a practical certainty with 1000 samples from a list of 5 numbers)
-        for value in [1,2,3,4,5]:
-            
-            # Count the appearances in each MC run
-            sample_counts = (sim_sample==value).sum(axis=0)
-            
-            # Assert >130 of this value sampled in each MC run
-            # (highly probable outcome)
-            self.assertTrue(
-                (sample_counts>130).all(),_probabalistic_errormsg
-            )
-            
-            # Assert <270 of this value sampled in each MC run
-            # (highly probable outcome)
-            self.assertTrue(
-                (sample_counts<270).all(),_probabalistic_errormsg
-            )
-
-        # According to the central limit theorem (since N=1000 satisfies CLT 
-        # eligibility), the probability that any one MC run  results in a 
-        # sample mean ≥.18 away from 3 is ~.01%. Across 100 independent MC
-        # runs, this is a bit less than 1% chance that at least one iteration 
-        # has a mean ≥.18. Perhaps good enough for a unit test.
-        self.assertTrue(
-            (np.abs(sim_sample.mean(axis=0)-3)<.18).all(),
-            "This probabilistic test observed an event that has a ~<1% chance "
-            "of occurring under expected operation. If you run the test again "
-            "and it fails, you likely have a problem."
-        )
+        # Assert the number of times each sample should appear under normal 
+        # operation and this seed
+        self.assertEqual((sim_sample==1).sum(),20128)
+        self.assertEqual((sim_sample==2).sum(),19871)
+        self.assertEqual((sim_sample==3).sum(),20095)
+        self.assertEqual((sim_sample==4).sum(),19879)
+        self.assertEqual((sim_sample==5).sum(),20027)
+        
+        # Assert the mean of all sampled values, given normal operation and 
+        # this seed.
+        self.assertAlmostEqual(sim_sample.mean(),2.99806)
 
     def test_correction_fn(self):
         """
@@ -187,7 +174,7 @@ class ROAMSModelTests(TestCase):
         # Create reference values that are only the sampled valued
         # emissions, wind-normalized emissions
         np.random.seed(1)
-        em, windnorm = self.model.get_aerial_survey_sample()
+        em_ref, windnorm_ref = self.model.get_aerial_survey_sample()
 
         # Correction function = 2x
         self.model.cfg.correction_fn = lambda em: em*2
@@ -196,11 +183,11 @@ class ROAMSModelTests(TestCase):
 
         # The emissions should be doubled after this correction
         np.testing.assert_equal(
-            em_2x,em*2
+            em_2x,em_ref*2
         )
         # No adjustments should modify the wind-normalized sample
         np.testing.assert_equal(
-            windnorm_2x,windnorm
+            windnorm_2x,windnorm_ref
         )
     
     def test_noise_fn(self):
@@ -220,11 +207,11 @@ class ROAMSModelTests(TestCase):
         # Create reference values that are only the sampled valued
         # emissions, wind-normalized emissions
         np.random.seed(1)
-        em, windnorm = self.model.get_aerial_survey_sample()
+        em_ref, windnorm_ref = self.model.get_aerial_survey_sample()
 
         # "noise" function is just multiplying by 2.
         self.model.cfg.noise_fn = lambda em: em*2
-        # Don't simulate error and apply noise
+        # Simulate error using this noise_fn
         self.model.cfg.simulate_error = True
         np.random.seed(1)
         em_2x, windnorm_2x = self.model.get_aerial_survey_sample()
@@ -232,12 +219,12 @@ class ROAMSModelTests(TestCase):
         # If the noise function multiplies by 2, the resulting 
         # sampled emissions should be multiplied by 2
         np.testing.assert_equal(
-            em_2x,em*2
+            em_2x,em_ref*2
         )
         
         # No adjustments should affect the wind-normalized sample
         np.testing.assert_equal(
-            windnorm_2x,windnorm
+            windnorm_2x,windnorm_ref
         )
     
     def test_handle_negatives(self):
@@ -257,7 +244,7 @@ class ROAMSModelTests(TestCase):
         # Create reference values that are only the sampled valued
         # emissions, wind-normalized emissions
         np.random.seed(1)
-        em, windnorm = self.model.get_aerial_survey_sample()
+        em_ref, windnorm_ref = self.model.get_aerial_survey_sample()
 
         # Set the handle_negative function to zero out negatives
         self.model.cfg.handle_negative = zero_out
@@ -268,13 +255,15 @@ class ROAMSModelTests(TestCase):
         np.random.seed(1)
         em_0, windnorm_0 = self.model.get_aerial_survey_sample()
 
+        # Assert that the remaining emissions is all 0: all the negative 
+        # values were turned into 0
         np.testing.assert_equal(
-            em_0,em*0
+            em_0,em_ref*0
         )
         
         # No adjustments should affect the wind-normalized sample
         np.testing.assert_equal(
-            windnorm_0,windnorm
+            windnorm_0,windnorm_ref
         )
     
     def test_make_aerial_prod_sample(self):
@@ -297,8 +286,8 @@ class ROAMSModelTests(TestCase):
         self.model.cfg.correction_fn = None         # No correction function: don't adjust the sampled emissions
         self.model.cfg.simulate_error = False       # Don't add random noise
         self.model.cfg.partial_detection_correction = True  # Tell it to add partial detection
-        self.model.cfg.PoD_fn = _half_pod           # Use PoD where >0 emissions -> P = .5
-        self.model.cfg.n_mc_samples = 1000          # Use 1000 MC samples
+        self.model.cfg.PoD_fn = _half_pod           # Use PoD where pos emissions get P = .5 -> add 1x emissions
+        self.model.cfg.n_mc_samples = 100          # Use 1000 MC samples
         self.model.cfg.num_wells_to_simulate = 10   # Simulate 10 wells only
 
         # Set a seed to control what the expected random behavior is
@@ -308,8 +297,8 @@ class ROAMSModelTests(TestCase):
         aerial_sample, pd_sample = self.model.make_aerial_prod_sample()
 
         # Resulting samples should both be [num wells to simulate] x [N MC samples]
-        self.assertEqual(aerial_sample.shape,(10,1000))
-        self.assertEqual(pd_sample.shape,(10,1000))
+        self.assertEqual(aerial_sample.shape,(10,100))
+        self.assertEqual(pd_sample.shape,(10,100))
 
         # Assert the partial detection emissions are exactly equal to 
         # the sampled emissions (what you get when PoD=.5 everywhere where 
@@ -318,12 +307,12 @@ class ROAMSModelTests(TestCase):
 
         # Assert that each underlying plume value appears in at least 
         # one sample
-        self.assertEqual((aerial_sample==35).sum(),494)
-        self.assertEqual((aerial_sample==48).sum(),320)
+        self.assertEqual((aerial_sample==35).sum(),51)
+        self.assertEqual((aerial_sample==48).sum(),40)
         
         # Assert that the mean is supposed to be based on the given seed.
         self.assertTrue(
-            aerial_sample[-2:,:].sum(axis=0).mean(),32.65
+            aerial_sample[-2:,:].sum(axis=0).mean(),37.05
         )
 
     def test_make_aerial_midstream_sample(self):
@@ -347,7 +336,7 @@ class ROAMSModelTests(TestCase):
         self.model.cfg.simulate_error = False       # Don't add random noise
         self.model.cfg.partial_detection_correction = True  # Tell it to add partial detection
         self.model.cfg.PoD_fn = _half_pod           # Use PoD where >0 emissions -> P = .5
-        self.model.cfg.n_mc_samples = 1000          # Use 1000 MC samples
+        self.model.cfg.n_mc_samples = 100          # Use 1000 MC samples
         self.model.cfg.num_wells_to_simulate = 10   # Simulate 10 wells only
 
         # Set a seed to control what the expected random behavior is
@@ -359,8 +348,8 @@ class ROAMSModelTests(TestCase):
         # Resulting samples should both be [2] x [N MC samples]
         # (for midstream, there's no associated infrastructure to simulated
         # with a sample of some size. Just the sources observed.)
-        self.assertEqual(aerial_sample.shape,(2,1000))
-        self.assertEqual(pd_sample.shape,(2,1000))
+        self.assertEqual(aerial_sample.shape,(2,100))
+        self.assertEqual(pd_sample.shape,(2,100))
 
         # Assert the partial detection emissions are exactly equal to 
         # the sampled emissions (what you get when PoD=.5 everywhere where 
@@ -369,11 +358,11 @@ class ROAMSModelTests(TestCase):
 
         # Assert that each underlying plume appears a fixed number of times 
         # based on the seed and current implementation
-        self.assertEqual((aerial_sample==210).sum(),249)
-        self.assertEqual((aerial_sample==242).sum(),190)
+        self.assertEqual((aerial_sample==210).sum(),27)
+        self.assertEqual((aerial_sample==242).sum(),26)
         
         # Assert that the mean is supposed to be based on the given seed.
-        self.assertEqual(aerial_sample.sum(axis=0).mean(),98.27)
+        self.assertEqual(aerial_sample.sum(axis=0).mean(),119.62)
 
     def test_raises_toofew_sim_data(self):
         """
@@ -396,15 +385,19 @@ class ROAMSModelTests(TestCase):
         self.model.prod_partial_detection_emissions[-2,:] = 35
         self.model.prod_partial_detection_emissions[-1,:] = 48
         
+        # Make up a simulated sample, where for each MC run it's [1,2,3,4,5]
+        # repeated
         self.model.simulated_sample = np.tile([1,2,3,4,5],(100,200)).T
 
         # transition point excludes highest simulated emission
+        # (leaving 800 simulated values in each MC run to fill 998 slots)
         # -> there will be too few simulated values to use
         self.model.cfg.prod_transition_point = 5 
         with self.assertRaises(IndexError):
             self.model.combine_prod_samples()
         
         # transition point no long excludes any simulated emissions
+        # (leaving 1000 simulated values in each MC run to fill 998 slots)
         # -> there are sufficient simulated emissions values
         self.model.cfg.prod_transition_point = 6
         self.model.combine_prod_samples()
@@ -415,6 +408,7 @@ class ROAMSModelTests(TestCase):
         given input aerial distributions
         """
         self.model.cfg.partial_detection_correction = True
+        self.model.cfg.prod_transition_point = None
         
         # Make the up the samples: pretend like the two aerial plumes
         # got sampled in each MC run
@@ -441,10 +435,16 @@ class ROAMSModelTests(TestCase):
             7567.55
         )
 
+        # Assert that the transition point is 20 for all iterations, as it 
+        # should under normal operation.
+        self.assertTrue((self.model.prod_tp==20).all())
+
         # Assert that the ordering of partial detection is maintained. 
         # Because each partial detection correction should be equal to 
         # the corresponding emissions (P=.5), just assert the values are 
         # equal wherever there's a partial detection.
+        
+        # pd_over0 = where partial detection emissions are >0
         pd_over0 = self.model.prod_partial_detection_emissions>0
         self.assertTrue(
             (   
@@ -487,6 +487,34 @@ class ROAMSModelTests(TestCase):
             self.model.combined_samples.sum(axis=0).mean(),
             3079.51
         )
+
+    def test_saves_config(self):
+        """
+        Assert that the config is saved to the `self.outputfolder` of the 
+        class, and that it's content is identical to TEST_CONFIG.
+        """
+        # Do the first 3 parts of perform_analysis()
+        self.model.make_samples()
+        self.model.combine_prod_samples()
+        self.model.compute_simulated_midstream_emissions()
+        
+        # Make the outpath folder if it doesn't exist.
+        if not os.path.exists(self.model.outfolder):
+            os.mkdir(self.model.outfolder)
+
+        # Save the input configs that were used for this run
+        self.model.save_config()
+
+        config = os.path.join(self.model.outfolder,"used_config.json")
+        # Assert the config was written to this path
+        self.assertTrue(os.path.exists(config))
+        
+        with open(config,"r") as f:
+            # Load content which may include non-JSON-safe windows paths ("C:\path\to\file.csv")
+            config = yaml.safe_load(f)
+
+        # Assert the loaded config is the same as that used for the test
+        self.assertEqual(config,TEST_CONFIG)
     
 if __name__=="__main__":
     import unittest
